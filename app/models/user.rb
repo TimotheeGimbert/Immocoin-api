e0b3ae598376a53cc41324900c102091b9ead1f4
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-08 18:35:50 +0100

    changing unless to allow_nil, tested locally OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
 
-  validates :first_name, length: { minimum: 2, maximum: 20 }, unless: nil?
-  validates :last_name, length: { minimum: 2, maximum: 20 }, unless: nil?
+  validates :first_name, length: { minimum: 2, maximum: 20 }, allow_nil: true
+  validates :last_name, length: { minimum: 2, maximum: 20 }, allow_nil: true
 
   has_many :properties
 end
