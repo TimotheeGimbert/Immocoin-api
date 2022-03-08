0b4b1af51a0801df9d3a770f194ae60c4377811d
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-08 17:38:54 +0100

    modifying typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
 
-  validates :first_name, length: { minimum: 2, maximum: 20 }, unless: fisrt_name.nil?
+  validates :first_name, length: { minimum: 2, maximum: 20 }, unless: first_name.nil?
   validates :last_name, length: { minimum: 2, maximum: 20 }, unless: last_name.nil?
 
   has_many :properties
